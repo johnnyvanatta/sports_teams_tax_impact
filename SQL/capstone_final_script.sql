b706be716181504cd87344865ca4c159304d9a46
Johnny Van Atta <jonathanvanatta5@gmail.com>
2025-08-20 11:54:20 -0500

    Wednesday Push One

--- SQL/capstone_final_script.sql
+++ SQL/capstone_final_script.sql
@@ -1,8 +1,8 @@
---- NSS CAPSTONE PROJECT: DO NORTH AMERICAN SPORTS TEAMS HAVE A GREATER ADVANTAGE OF WINNING IF THEY ARE LOCATED IN STATES WITH NO INCOME TAX VS TEAMS IN STATES THAT DO?
+-- NSS CAPSTONE PROJECT: DO NORTH AMERICAN SPORTS TEAMS HAVE A GREATER ADVANTAGE OF WINNING IF THEY ARE LOCATED IN STATES WITH NO INCOME TAX VS TEAMS IN STATES THAT DO?
 
--- ONLY USING LEAGUE WHO HAVE A STRICT SALARY CAP. MLB AND NBA ARE TAKEN OUT OF CONSIDERATION DUE TO LEAGUE WIDE FLEXIBILITY ON SPENDING
+-- ONLY USING LEAGUES WHO HAVE A STRICT SALARY CAP. MLB AND NBA ARE TAKEN OUT OF CONSIDERATION DUE TO LEAGUE WIDE FLEXIBILITY ON SPENDING SO IM ONLY LOOKING AT NFL AND NHL
 
--- Tables brought in
+-- 4 Main yables brought in
 
 SELECT *
 FROM teams
@@ -16,14 +16,54 @@ FROM taxes
 SELECT *
 FROM salary_caps
 
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 -- Combined base table to look at
-SELECT league, season, team_id, team_name, team_state, team_country, w, l, otl, pct, net_pts, playoffs, conf_champs, league_champs, season_rank, salary_cap, combined_tax, tax_result 
+SELECT league, season, team_id, team_name, team_state, team_country, w, l, otl, pct, net_pts, season_rank, playoffs, 
+conf_champs, league_champs, tax_result, federal_tax, state_tax, combined_tax, salary_cap, 
+(combined_tax * salary_cap) AS total_taxed,  (salary_cap - (combined_tax * salary_cap)) AS adjusted_cap
 FROM locations
 	INNER JOIN taxes USING(team_state, team_country)
 	INNER JOIN teams USING(league, season, team_id, team_name)
 	INNER JOIN salary_caps USING(league, season)
 WHERE league = 'NFL' OR league = 'NHL'
 ORDER BY season ASC, league ASC
+
+
+
+
+
+
+
+--- ANALYSIS TABLE ---
+SELECT league, season, team_id, team_name, team_state, team_country, w, l, otl, pct, net_pts, season_rank, playoffs, conf_champs, league_champs, tax_result, federal_tax, state_tax, combined_tax, salary_cap, 
+(combined_tax * salary_cap) AS total_taxed,  (salary_cap - (combined_tax * salary_cap)) AS adjusted_cap, MIN(salary_cap - (combined_tax * salary_cap)) OVER (PARTITION BY league, season) AS true_cap,
+ ((salary_cap - (combined_tax * salary_cap)) - MIN(salary_cap - (combined_tax * salary_cap)) OVER (PARTITION BY league, season)) AS cap_diff,
+ROUND((((salary_cap - (combined_tax * salary_cap))  - MIN(salary_cap - (combined_tax * salary_cap)) OVER (PARTITION BY league, season)) / MIN(salary_cap - (combined_tax * salary_cap)) OVER (PARTITION BY league, season))::numeric, 3) AS pct_diff
+FROM locations
+	INNER JOIN taxes USING(team_state, team_country)
+	INNER JOIN teams USING(league, season, team_id, team_name)
+	INNER JOIN salary_caps USING(league, season)
+WHERE league IN('NFL', 'NHL')
+ORDER BY season ASC, league ASC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -51,24 +91,60 @@ WHERE tax_result = 'Y'
 AND (league = 'NHL' OR league = 'NFL')
 ORDER BY team_name ASC
 
-SELECT *
+
+
+
+-- Q2. Which state/province has the highest combined tax rate each year
+SELECT table1.season, table1.team_state, table1.team_country, table1.combined_tax
+FROM taxes table1
+WHERE table1.combined_tax = (
+	SELECT MAX(table2.combined_tax)
+	FROM taxes table2
+	WHERE table2.season = table1.season
+)
+ORDER BY table1.season
+
+
+
+
+
+
+-- Q3. What is the average win percentage of teams grouped by state? 
+-- Mass has the highest with 65% (Boston Bruins and New England Patriots)
+SELECT team_state, COUNT(DISTINCT team_name) AS num_of_teams, ROUND(AVG(pct), 2) AS avg_win_pct
 FROM teams
-WHERE season = '2005-2006' AND league = 'NHL'
+	INNER JOIN locations USING(league, team_id, team_name)
+WHERE league IN('NFL','NHL')
+GROUP BY team_state
+ORDER BY avg_win_pct DESC
 
 
-SELECT DISTINCT(team_id)
+
+
+-- Q4. Which states have produced the most playoff appearances, conference champions and league champions?
+-- Penn has the most in every catergory 
+SELECT table2.team_state, COUNT(DISTINCT team_name) AS team_num, 
+	SUM(CASE WHEN table1.playoffs = 'Y' THEN 1 ELSE 0 END) AS playoff_apps,
+	SUM(CASE WHEN table1.conf_champs = 'Y' THEN 1 ELSE 0 END) AS conf_champs,
+	SUM(CASE WHEN table1.league_champs = 'Y' THEN 1 ELSE 0 END) AS league_champs
+FROM teams AS table1 
+	INNER JOIN locations AS table2 USING(league, team_id, team_name) 
+WHERE league IN('NFL', 'NHL') 
+GROUP BY table2.team_state 
+ORDER BY league_champs DESC
+
+
+
+
+
+-- Q5. Compare average win % of teams in 0% state tax states vs. taxed states.
+SELECT tax_result, ROUND(AVG(pct), 2) AS win_pct
 FROM locations
-WHERE league = 'NHL'
-
--- Q2.
-
-
-
-
-
-
-
---- Q3.
+	INNER JOIN teams USING(league, team_id, team_name)
+	INNER JOIN taxes USING(season, team_state, team_country)
+WHERE league IN('NFL', 'NHL')
+GROUP BY tax_result
+ORDER BY win_pct DESC
 
 
 
@@ -76,12 +152,15 @@ WHERE league = 'NHL'
 
 
 
-
-
-
-
---- Q4.
-
+-- Q6. Do tax-free states win more league championships?
+SELECT tax_result,
+	SUM(CASE WHEN league_champs = 'Y' THEN 1 ELSE 0 END) AS champions
+FROM locations
+	INNER JOIN teams USING(league, team_id, team_name)
+	INNER JOIN taxes USING(season, team_state, team_country)
+WHERE league IN('NFL', 'NHL')
+GROUP BY tax_result
+ORDER BY champions DESC
 
 
 
@@ -143,13 +222,9 @@ ORDER BY season ASC, combined_tax DESC
 
 
 
-SELECT *, (salary_cap * combined_tax) AS total_taxed, (salary_cap - (salary_cap * combined_tax)) AS adjusted_cap
-FROM locations
-	INNER JOIN taxes USING(team_state, team_country)
-	INNER JOIN teams USING(league, season, team_id, team_name)
-	INNER JOIN salary_caps USING(league, season)
-WHERE league = 'NHL' AND season = '2024-2025'
-ORDER BY adjusted_cap ASC
+
+
+
 
 
 
